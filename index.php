1b776dd1e5834e926d62b7cd91c3c86910d13d0f
camzu1998 <kamillanger4@gmail.com>
2016-12-16 19:51:55 +0200

    Signed-off-by: camzu1998 <kamillanger4@gmail.com>

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+    <link href="https://fonts.googleapis.com/css?family=Amatic+SC|Courgette|Exo" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="style.css"/>
+    </head>
+
+    <body>
+        <container>
+            <header><h2>Obiektyw</h2><h4>Wioleta Langer</h4></header>
+            <content>
+                <div id="sidebar">
+                    <ol>
+                        <li>O mnie</li>
+                        <li>Życiorys</li>
+                        <li>Cennik</li>
+                        <li>Kontakt</li>
+                    </ol>
+                </div>
+            </content>
+            <footer></footer>
+        </container>
+    </body>
+</html>
