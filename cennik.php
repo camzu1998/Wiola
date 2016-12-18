795d6a05bd44e66d44b82b6f447d0298f3d2da6a
camzu1998 <kamillanger4@gmail.com>
2016-12-18 13:12:33 +0200

    18.12.2016

---
+++ cennik.php
@@ -0,0 +1,36 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+    <link href="https://fonts.googleapis.com/css?family=Amatic+SC|Courgette|Exo" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="style.css"/>
+    </head>
+
+    <body>
+        <container>
+            <header><h2><a href="index.php" class="Obiektyw">Obiektyw</a></h2><h4>Wioleta Langer</h4></header>
+            <content>
+                <div id="sidebar">
+                    <ol>
+                        <br>
+                        <li><a href="omnie.php">O mnie</a></li>
+                        <li><a href="zyciorys.php">Życiorys</a></li>
+                        <li><a href="cennik.php">Cennik</a></li>
+                        <li><a href="kontakt.php">Kontakt</a></li>
+                    </ol>
+                </div>
+                <div id="glowna">
+                    <table class="tabelka">
+                        <tr class="Pierwsza"><td>Sesja standard</td><td>Sesja premium</td><td>Sesja VIP</td></tr>
+                        <tr class="reszta"><td>350zł</td><td>600zł</td><td>850zł</td></tr>
+                        <tr class="reszta"><td>Czas trwania sesji:<br>1-2 godziny</td><td>Czas trwania sesji:<br>3-4 godziny</td><td>Czas trwania sesji:<br>5-6 godzin</td></tr>
+                        <tr class="reszta"><td>Sesja w studiu</td><td>Sesja w studiu lub w plenerze</td><td>Sesja w studiu lub w plenerze</td></tr>
+                        <tr class="reszta"><td>10 wybranych ujęć nagranych na pendrive</td><td>20 wybranych ujęć nagranych na pendrive</td><td>30 wybranych ujęć nagranych na pendrive</td></tr>
+                    </table>
+                </div>
+            </content>
+            <footer></footer>
+        </container>
+    </body>
+</html>
---
+++ kontakt.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+    <link href="https://fonts.googleapis.com/css?family=Amatic+SC|Courgette|Exo" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="style.css"/>
+    </head>
+
+    <body>
+        <container>
+            <header><h2><a href="index.php" class="Obiektyw">Obiektyw</a></h2><h4>Wioleta Langer</h4></header>
+            <content>
+                <div id="sidebar">
+                    <ol>
+                        <br>
+                        <li><a href="omnie.php">O mnie</a></li>
+                        <li><a href="zyciorys.php">Życiorys</a></li>
+                        <li><a href="cennik.php">Cennik</a></li>
+                        <li><a href="kontakt.php">Kontakt</a></li>
+                    </ol>
+                </div>
+                <div id="glowna">
+                    <p style="text-align:center; font-size:90px; margin-top: 15%;">NIE MA</p>
+                </div>
+            </content>
+            <footer></footer>
+        </container>
+    </body>
+</html>
